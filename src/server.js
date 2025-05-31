67472bc31af33907aaf829414134f6575dcf84ed
christianeliseoisip <christianeliseoisip@laverdad.edu.ph>
2025-05-31 23:28:30 +0800

    Updated cors

--- src/server.js
+++ src/server.js
@@ -11,7 +11,10 @@ const PORT = process.env.PORT || 5000;
 connectDB(); // Connect to MongoDB
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: "https://fieldmate-backend.onrender.com" 
+}));
+
 app.use(express.json()); // To parse JSON request bodies
 
 // Routes
