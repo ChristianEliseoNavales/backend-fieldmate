d60600d5560d44fbfd03311dafbaf26f6ae8fec6
christianeliseoisip <christianeliseoisip@laverdad.edu.ph>
2025-05-31 23:38:41 +0800

    Minor changes

--- src/server.js
+++ src/server.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 5000;
 connectDB(); // Connect to MongoDB
 
 const allowedOrigins = [
-  "http://localhost:3000", // your local frontend URL
+  `http://localhost:${PORT}`, // your local frontend URL
   "https://your-frontend.onrender.com" // production frontend URL
 ];
 
