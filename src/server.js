192c873f6870f61959431b199708babec4da81bc
christianeliseoisip <christianeliseoisip@laverdad.edu.ph>
2025-06-08 16:47:24 +0800

    Testing Auto Deploy on commit

--- src/server.js
+++ src/server.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
-const connectDB = require('./config/db'); // Import the database connection function
+const connectDB = require('./config/db'); 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-connectDB(); // Connect to MongoDB
+connectDB(); 
 
 const allowedOrigins = [
   "http://localhost:5173",
@@ -26,7 +26,7 @@ app.use(cors({
 }));
 
 
-app.use(express.json()); // To parse JSON request bodies
+app.use(express.json()); 
 
 // Routes
 const journalRoutes = require('./routes/journalRoutes');
@@ -43,7 +43,6 @@ app.use('/api/attendance', attendanceRoutes);
 app.use("/api", authRoutes);
 app.use("/api", companyRoutes); 
 
-// Start server
 app.listen(PORT, () => {
   console.log(`🚀 Server running on http://localhost:${PORT}`);
 });
